2c04aef50eb671240dcafc27240d5c4151587a71
RYP12 <ramonye2002@gmail.com>
2025-02-19 10:23:35 +0100

    ajustes

--- 1º DAW/PROYECTOS/LeagueOfLegends/src/main/java/modelos/Region.java
+++
@@ -1,7 +0,0 @@
-package modelos;
-
-public enum Region {
-    DEMACIA, NOXUS, JONIA, SHURIMA,
-    TARGON, FREIJORD, PILTOVER, ZAUM, BANDLE,
-    AGUAS_ESTANCADAS, DESCONOCIDA
-}
--- 1º DAW/PROYECTOS/LeagueOfLegends/src/main/java/modelos/TipoHabilidad.java
+++
@@ -1,6 +0,0 @@
-package modelos;
-
-public enum TipoHabilidad {
-    DANYO, CURACION, BUFO_VIDA,
-    BUFO_DEFENSA, BUFO_PODER
-}
